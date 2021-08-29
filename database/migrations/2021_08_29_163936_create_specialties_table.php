3d660d4e7767cd3749e80883f8e882e32fb9464a
Marcos Custodio <mpcustodio91@gmail.com>
2021-08-29 14:02:13 -0300

    Criação da estrutura de banco de dados e relacionamentos

---
+++ database/migrations/2021_08_29_163936_create_specialties_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecialtiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specialties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->timestamps();
+        });
+
+        Schema::create('dentists_specialties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('dentist_id')->constrained('dentists');
+            $table->foreignId('specialtie_id')->constrained('specialties');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dentists_specialties');
+        Schema::dropIfExists('specialties');
+    }
+}
